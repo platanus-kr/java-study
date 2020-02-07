f689cb2cbd69fb7a0f33e1857a106dd2ae155093
platanus <platanus@canxan.com>
2020-02-07 23:37:36 +0900

    mybatis configuration

--- board/src/main/java/board/ServletInitializer.java
+++ board/src/main/java/board/ServletInitializer.java
@@ -1,4 +1,4 @@
-package demo2;
+package board;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Demo3Application.class);
+		return application.sources(BoardApplication.class);
 	}
 
 }

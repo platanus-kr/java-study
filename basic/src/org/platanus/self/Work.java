ff4d9cde2defd4fdaff3d131c22d5f84b08a60c2
platanus <platanus@canxan.com>
2020-02-12 21:09:32 +0900

    practice

---
+++ basic/src/org/platanus/self/Work.java
@@ -0,0 +1,11 @@
+package org.platanus.self;
+
+import org.platanus.self.WebServer.Controller;
+
+public class Work {
+
+    public static void main(String[] args) {
+        System.out.println("work");
+        Controller.ServerCheck();
+    }
+}

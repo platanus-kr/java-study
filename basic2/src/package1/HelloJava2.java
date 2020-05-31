59bc192d3ed421d1c426a488c43cfff416111d76
platanus <platanus@canxan.com>
2020-05-31 22:02:12 +0900

    20200531

---
+++ basic2/src/package1/HelloJava2.java
@@ -0,0 +1,7 @@
+package package1;
+
+public class HelloJava2 {
+    public static void main(String[] args) {
+        System.out.println("yesy");
+    }
+}
---
+++ basic2/src/package2/CharacterEx1.java
@@ -0,0 +1,9 @@
+package package2;
+
+public class CharacterEx1 {
+    public static void main(String[] args) {
+        char ch1 = 'A';
+        System.out.println(ch1);
+        System.out.println((int)ch1);
+    }
+}

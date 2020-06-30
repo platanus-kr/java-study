057be50bdfdfa19d7fd656edda105ffa1489e106
platanus <platanus@canxan.com>
2020-06-30 11:48:42 +0900

    20200630

---
+++ basic2/src/package2/ForExample1.java
@@ -0,0 +1,13 @@
+package package2;
+
+public class ForExample1 {
+    public static void main(String[] args) {
+        int i;
+        int sum;
+        for(i = 1, sum = 0; i <= 10; i++){
+            sum +=i;
+        }
+        System.out.println(sum);
+    }
+
+}

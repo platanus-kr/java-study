a40a5889ef87cb5327a69030fcf69d747a00a613
platanus <platanus@canxan.com>
2021-01-07 23:31:54 +0900

    feat(chap03): Make MemberRegisterService

---
+++ spring5/sp5-chap03/src/main/java/spring/MemberRegisterService.java
@@ -0,0 +1,26 @@
+package spring;
+
+import java.time.LocalDateTime;
+
+public class MemberRegisterService {
+    private MemberDao memberDao;
+
+    public MemberRegisterService(MemberDao memberDao){
+        this.memberDao = memberDao;
+    }
+
+    public Long regist(RegisterRequest request){
+        Member member = memberDao.selectByEmail(request.getEmail());
+        if(member !=null){
+            throw new DuplicateMemberException("duplicated email " +request.getEmail());
+        }
+        Member newMember = new Member(
+            request.getEmail(),
+            request.getPassword(),
+            request.gerName(),
+            LocalDateTime.now()
+        );
+        memberDao.insert(newMember);
+        return newMember.getId();
+    }
+}

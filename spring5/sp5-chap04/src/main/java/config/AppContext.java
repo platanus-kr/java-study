1b3513a637c2b12cf3eb74c43f8716cd938e4e4d
platanus <platanus@canxan.com>
2021-02-02 14:10:27 +0900

    feat(chap04): Use @Qualifier

--- spring5/sp5-chap04/src/main/java/config/AppContext.java
+++ spring5/sp5-chap04/src/main/java/config/AppContext.java
@@ -1,5 +1,6 @@
 package config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import spring.ChangePasswordService;
@@ -34,28 +35,18 @@ public class AppContext {
     }
 
     @Bean
+    @Qualifier("printer")
     public MemberListPrinter listPrinter() {
-        // constructor 방식
-        // 인자가 두 개 일때도 똑같이 넘기면 된다.
-        // bean 객체를 생성하는 시점에 필요한 모든 의존 객체를 주입받는다.
-        // 그래서 객체를 사용할 때 완전한 상태로 사용 할 수 있다.
-        // 그러나 넘겨야하는 객체가 많을 때는 일일이 확인해야하는 단점이 있다.
-        // return new MemberListPrinter(memberDao(), memberPrinter());
+        return new MemberListPrinter();
+    }
 
-        // @Autowired 적용
+    @Bean
+    public MemberListPrinter listPrinter2() {
         return new MemberListPrinter();
     }
 
     @Bean
     public MemberInformationPrinter memberInformationPrinter() {
-        // setter 방식
-        // bean 객체를 생성하는 시점에 필요한 객체를 모두 알 수 있다.
-        // 그러나 필요한 의존객체를 전달하지 않아도 bean 객체가 생성되기 때문에 NPE가 발생할 수 있다.
-//        MemberInformationPrinter informationPrinter = new MemberInformationPrinter();
-        // 아래의 경우는 메소드에 자동주입 어노테이션.
-        // informationPrinter.setMemberDao(memberDao());
-        // informationPrinter.setPrinter(memberPrinter());
-//        return informationPrinter;
         return new MemberInformationPrinter();
     }
 
--- spring5/sp5-chap04/src/main/java/spring/MemberListPrinter.java
+++ spring5/sp5-chap04/src/main/java/spring/MemberListPrinter.java
@@ -2,6 +2,7 @@ package spring;
 
 import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class MemberListPrinter {
 
@@ -27,6 +28,7 @@ public class MemberListPrinter {
         this.memberDao = memberDao;
     }
     @Autowired
+    @Qualifier("printer")
     public void setPrinter(MemberPrinter printer) {
         this.printer = printer;
     }

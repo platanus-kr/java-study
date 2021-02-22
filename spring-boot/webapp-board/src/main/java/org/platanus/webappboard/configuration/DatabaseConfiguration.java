0e8289868170e7c214cf9bcd78899e64131cb9b6
platanus <platanus@canxan.com>
2021-02-23 03:41:49 +0900

    feat(board): Add Bean as MyBatis

--- spring-boot/webapp-board/src/main/java/org/platanus/webappboard/configuration/DatabaseConfiguration.java
+++ spring-boot/webapp-board/src/main/java/org/platanus/webappboard/configuration/DatabaseConfiguration.java
@@ -4,7 +4,12 @@ package org.platanus.webappboard.configuration;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import javax.sql.DataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -12,6 +17,9 @@ import org.springframework.context.annotation.PropertySource;
 @Configuration
 @PropertySource("classpath:/application.properties")
 public class DatabaseConfiguration {
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource.hikari")
@@ -21,9 +29,23 @@ public class DatabaseConfiguration {
 
     @Bean
     public DataSource dataSource() throws Exception {
-        DataSource dataSource = new HikariDataSource(hikariConfig());
-        System.out.println(dataSource.toString());
-        return dataSource();
+        return new HikariDataSource(hikariConfig());
+
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setMapperLocations(
+            applicationContext.getResource("class:/mapper/**/sql-*.xml")
+        );
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
     }
 
 }

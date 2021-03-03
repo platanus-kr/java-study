e0efc9ce3ffc54875d887ffb4cf96e3ffe1efa6e
platanus <platanus@canxan.com>
2021-03-04 02:11:12 +0900

    feat(board): Add Interceptor

---
+++ spring-boot/webapp-board/src/main/java/org/platanus/webappboard/configuration/WebMVCConfiguration.java
@@ -0,0 +1,15 @@
+package org.platanus.webappboard.configuration;
+
+import org.platanus.webappboard.interceptor.LoggerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMVCConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggerInterceptor());
+
+    }
+
+}

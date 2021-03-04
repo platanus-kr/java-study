45f17d4a68aea66c55c55b4cfecc27b476a62454
platanus <platanus@canxan.com>
2021-03-04 18:43:36 +0900

    feat(board): Add TransactionAspect

--- spring-boot/webapp-board/src/main/java/org/platanus/webappboard/configuration/DatabaseConfiguration.java
+++ spring-boot/webapp-board/src/main/java/org/platanus/webappboard/configuration/DatabaseConfiguration.java
@@ -13,9 +13,13 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @PropertySource("classpath:/application.properties")
+@EnableTransactionManagement
 public class DatabaseConfiguration {
 
     @Autowired
@@ -53,6 +57,12 @@ public class DatabaseConfiguration {
     @ConfigurationProperties(prefix = "mybatis.configuration")
     public org.apache.ibatis.session.Configuration mybatisConfig() {
         return new org.apache.ibatis.session.Configuration();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() throws Exception {
+        return new DataSourceTransactionManager(dataSource());
+
     }
 
 }

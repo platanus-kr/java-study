a7a0ac07841ff76874bfb254cb2ba5c454106461
platanus <platanus@canxan.com>
2021-03-13 22:00:58 +0900

    feat(designpattern): Make singleton pattern

---
+++ java/basic/src/org/platanus/designpattern/singleton/Company.java
@@ -0,0 +1,17 @@
+package org.platanus.designpattern.singleton;
+
+public class Company {
+
+    private static Company instance = new Company();
+
+    private Company() {
+    }
+
+    public static Company getInstance() {
+        if (instance == null) {
+            instance = new Company();
+        }
+        return instance;
+    }
+
+}

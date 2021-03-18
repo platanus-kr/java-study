870d5aeeb87b2dd19c4c0b351c7cf6337e9e78ae
platanus <platanus@canxan.com>
2021-03-18 11:27:11 +0900

    feat(collection): Make DAO

---
+++ java/basic/src/org/platanus/collection/dao/Member.java
@@ -0,0 +1,33 @@
+package org.platanus.collection.dao;
+
+public class Member {
+    private int memberId;
+    private String memberName;
+
+    public Member(int memberId, String memberName) {
+        this.memberId = memberId;
+        this.memberName = memberName;
+    }
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    @Override
+    public String toString() {
+        String s = memberName + "Member ID is " + memberId + ".";
+        return s;
+    }
+}

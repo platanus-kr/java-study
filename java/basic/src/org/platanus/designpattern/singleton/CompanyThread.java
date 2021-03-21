c3d6abb1fb4a8f18b07ad3a8155b4937f26462f6
platanus <platanus@canxan.com>
2021-03-21 19:51:50 +0900

    feat(singleton): Add synchronized keyword

--- java/basic/src/org/platanus/designpattern/singleton/CompanyThread.java
+++ java/basic/src/org/platanus/designpattern/singleton/CompanyThread.java
@@ -2,28 +2,30 @@ package org.platanus.designpattern.singleton;
 
 public class CompanyThread {
 
-    //        private static CompanyThread instance = null;
-    private static CompanyThread instance = new CompanyThread();
-//    private int counter = 0;
+    private static CompanyThread instance = null;
+    //    private static CompanyThread instance = new CompanyThread();
+    private int counter = 0;
 
     private CompanyThread() {
     }
 
-    public static CompanyThread getInstance() {
-//        if (instance == null) {
-//            try {
-//                Thread.sleep(1000);
-//            } catch (InterruptedException e) {
-//            }
-//            instance = new CompanyThread();
-//        }
+    public synchronized static CompanyThread getInstance() {
+        if (instance == null) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+            instance = new CompanyThread();
+        }
         return instance;
     }
 
     public void foodDistribute(String foodDist) {
-//        counter++;
-//        System.out.println(foodDist + ". total counter " + counter);
-        System.out.println(foodDist);
+        synchronized (this) {
+            counter++;
+            System.out.println(foodDist + ". total counter " + counter);
+        }
+//        System.out.println(foodDist);
     }
 
     @Override

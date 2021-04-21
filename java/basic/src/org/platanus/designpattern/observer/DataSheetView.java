0d5069ee1048af3e1e9b0b4096d1f676b4f1e060
platanus <platanus@canxan.com>
2021-04-21 16:51:31 +0900

    feat(observerpattern): Make init.

---
+++ java/basic/src/org/platanus/designpattern/observer/DataSheetView.java
@@ -0,0 +1,33 @@
+package org.platanus.designpattern.observer;
+
+import java.util.List;
+
+public class DataSheetView {
+
+    private ScoreRecord scoreRecord;
+    private int viewCount;
+
+    public DataSheetView(ScoreRecord scoreRecord, int viewCount) {
+        this.scoreRecord = scoreRecord;
+        this.viewCount = viewCount;
+    }
+
+    public void update() {
+        List<Integer> record = scoreRecord.getScoreRecord();
+        displayScores(record, viewCount);
+    }
+
+    public void displayScores(List<Integer> record, int viewCount) {
+        System.out.print("List of " + viewCount + " entries:");
+        int i = 0;
+        while (!record.isEmpty()) {
+            if (viewCount == i || i >= record.size()) {
+                break;
+            }
+            System.out.print(record.get(i) + " ");
+            i += 1;
+        }
+        System.out.println(" ");
+    }
+
+}

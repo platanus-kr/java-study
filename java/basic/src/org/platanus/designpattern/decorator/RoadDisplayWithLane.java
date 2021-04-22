d6fadff4e8f72458547ced6a9e67b47363d34dbc
platanus <platanus@canxan.com>
2021-04-22 10:49:34 +0900

    feat(decoratorpattern): Make init.

---
+++ java/basic/src/org/platanus/designpattern/decorator/RoadDisplayWithLane.java
@@ -0,0 +1,15 @@
+package org.platanus.designpattern.decorator;
+
+public class RoadDisplayWithLane extends RoadDisplay {
+
+    public void draw() {
+        super.draw();
+        drawLane();
+    }
+
+    private void drawLane() {
+        System.out.println("Show the lane.");
+    }
+
+
+}

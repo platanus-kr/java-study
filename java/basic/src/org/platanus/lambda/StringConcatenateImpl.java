8152f49194dbcf04ae4f2f3ae20028ae657247a0
platanus <platanus@canxan.com>
2021-04-26 17:02:51 +0900

    feat(lambda): Make proposal

---
+++ java/basic/src/org/platanus/lambda/StringConcatenateImpl.java
@@ -0,0 +1,21 @@
+package org.platanus.lambda;
+
+
+class Test {
+
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "World";
+        StringConcatenateImpl concatenate = new StringConcatenateImpl();
+        concatenate.makeString(str1, str2);
+    }
+}
+
+public class StringConcatenateImpl implements StringConcatgenate {
+
+    @Override
+    public void makeString(String s1, String s2) {
+        System.out.println(s1 + ", " + s2);
+    }
+}
+
---
+++ java/basic/src/org/platanus/lambda/StringConcatgenate.java
@@ -0,0 +1,7 @@
+package org.platanus.lambda;
+
+public interface StringConcatgenate {
+
+    public void makeString(String s1, String s2);
+
+}

b5c2a9a4f1cfa2bad19760c5b50ca5dec690cdd3
platanus <platanus@canxan.com>
2021-05-18 17:48:09 +0900

    feat(serialization): Making..

---
+++ java/basic/src/org/platanus/io/Serialization/SerialTest.java
@@ -0,0 +1,53 @@
+package org.platanus.io.Serialization;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+class Person implements Serializable {
+
+    //    private static final long serialVersionUID = -2345L;
+    String name;
+    String job;
+
+    public Person() {
+    }
+
+    public Person(String name, String job) {
+        this.name = name;
+        this.job = job;
+    }
+
+    public String toString() {
+        return name + ", " + job;
+    }
+}
+
+public class SerialTest {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        Person personKang = new Person("Kang", "CEO");
+        Person personPak = new Person("Pak", "CTO");
+
+        try (FileOutputStream fos = new FileOutputStream("serial.out");
+            ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(personKang);
+            oos.writeObject(personPak);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ///
+        try (FileInputStream fis = new FileInputStream("serial.out");
+            ObjectInputStream ois = new ObjectInputStream(fis)) {
+            Person s1 = (Person) ois.readObject();
+            Person s2 = (Person) ois.readObject();
+            System.out.println(s1);
+            System.out.println(s2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

ab0b8b84282cecabe19256a1f431544f99819efd
platanus <platanus@canxan.com>
2021-06-11 20:59:21 +0900

    feat(springboot): Make websocket ing...

--- spring-boot/42project/study/demo_03/src/main/java/com/example/payroll/Employee.java
+++ spring-boot/42project/study/demo_03/src/main/java/com/example/payroll/Employee.java
@@ -4,17 +4,19 @@ import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Version;
+import net.minidev.json.annotate.JsonIgnore;
 
 @Entity
 public class Employee {
 
-    private @Id
-    @GeneratedValue
-    Long id;
+    private @Id @GeneratedValue Long id;
 
     private String firstName;
     private String lastName;
     private String description;
+
+    private @Version @JsonIgnore Long version;
 
     private Employee() {
     }
@@ -37,12 +39,13 @@ public class Employee {
         return Objects.equals(id, employee.id) &&
             Objects.equals(firstName, employee.firstName) &&
             Objects.equals(lastName, employee.lastName) &&
-            Objects.equals(description, employee.description);
+            Objects.equals(description, employee.description) &&
+            Objects.equals(version, employee.version);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, firstName, lastName, description);
+        return Objects.hash(id, firstName, lastName, description, version);
     }
 
     public Long getId() {
@@ -77,6 +80,14 @@ public class Employee {
         this.description = description;
     }
 
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
     @Override
     public String toString() {
         return "Employee{" +
@@ -84,6 +95,7 @@ public class Employee {
             ", firstName='" + firstName + '\'' +
             ", lastName='" + lastName + '\'' +
             ", description='" + description + '\'' +
+            ", version=" + version +
             '}';
     }
 }

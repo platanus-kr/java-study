14940c3aa2967e0a2c0967ec4e94ababf3c79797
platanus <platanus@canxan.com>
2021-06-11 12:15:11 +0900

    feat(springboot)

---
+++ spring-boot/42project/study/demo03/src/main/js/app.js
@@ -0,0 +1,72 @@
+'use strict';
+
+// tag::vars[]
+const React = require('react'); // <1>
+const ReactDOM = require('react-dom'); // <2>
+const client = require('./client'); // <3>
+// end::vars[]
+
+// tag::app[]
+class App extends React.Component { // <1>
+
+	constructor(props) {
+		super(props);
+		this.state = {employees: []};
+	}
+
+	componentDidMount() { // <2>
+		client({method: 'GET', path: '/api/employees'}).done(response => {
+			this.setState({employees: response.entity._embedded.employees});
+		});
+	}
+
+	render() { // <3>
+		return (
+			<EmployeeList employees={this.state.employees}/>
+		)
+	}
+}
+// end::app[]
+
+// tag::employee-list[]
+class EmployeeList extends React.Component{
+	render() {
+		const employees = this.props.employees.map(employee =>
+			<Employee key={employee._links.self.href} employee={employee}/>
+		);
+		return (
+			<table>
+				<tbody>
+					<tr>
+						<th>First Name</th>
+						<th>Last Name</th>
+						<th>Description</th>
+					</tr>
+					{employees}
+				</tbody>
+			</table>
+		)
+	}
+}
+// end::employee-list[]
+
+// tag::employee[]
+class Employee extends React.Component{
+	render() {
+		return (
+			<tr>
+				<td>{this.props.employee.firstName}</td>
+				<td>{this.props.employee.lastName}</td>
+				<td>{this.props.employee.description}</td>
+			</tr>
+		)
+	}
+}
+// end::employee[]
+
+// tag::render[]
+ReactDOM.render(
+	<App />,
+	document.getElementById('react')
+)
+// end::render[]

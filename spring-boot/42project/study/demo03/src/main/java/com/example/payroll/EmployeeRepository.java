c1997902dd47ad3d6a31fa7cb2973a7b033d127b
platanus <platanus@canxan.com>
2021-06-11 16:23:17 +0900

    feat(springboot): Make employee create and delete UI

--- spring-boot/42project/study/demo03/src/main/java/com/example/payroll/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.payroll;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-
-}
---
+++ spring-boot/42project/study/demo_03/src/main/java/com/example/payroll/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.example.payroll;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
+
+}

45b8884b595e41f364b5617a1d49333035e59d22
platanus <platanus@canxan.com>
2022-05-23 23:52:34 +0900

    feat(jpa): init.

---
+++ jpa/jpa01-basic/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,17 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        em.close();
+
+        emf.close();
+    }
+}

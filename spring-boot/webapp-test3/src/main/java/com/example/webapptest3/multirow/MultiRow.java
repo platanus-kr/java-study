123b06f83e815e8546b6b20a94ef1da20e81cd3d
platanus <platanus@canxan.com>
2022-07-03 11:45:12 +0900

    add: form-data array mapping

---
+++ spring-boot/webapp-test3/src/main/java/com/example/webapptest3/multirow/MultiRow.java
@@ -0,0 +1,10 @@
+package com.example.webapptest3.multirow;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class MultiRow {
+    private List<Row> rows;
+}
---
+++ spring-boot/webapp-test3/src/main/java/com/example/webapptest3/multirow/Row.java
@@ -0,0 +1,9 @@
+package com.example.webapptest3.multirow;
+
+import lombok.Data;
+
+@Data
+public class Row {
+    private int id;
+    private String value;
+}

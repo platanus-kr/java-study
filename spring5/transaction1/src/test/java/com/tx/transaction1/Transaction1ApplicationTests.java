1ab4b1ef942aa2ce4cfaa52b9ca867fef1aa816f
platanus <platanus@canxan.com>
2022-08-18 10:15:14 +0900

    add: Transaction

---
+++ spring5/transaction1/src/test/java/com/tx/transaction1/Transaction1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tx.transaction1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Transaction1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

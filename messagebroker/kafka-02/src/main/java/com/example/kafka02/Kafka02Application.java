26bb5ba139c1aa27ff799474095106f29fea1944
platanus <platanus@canxan.com>
2023-06-19 16:18:45 +0900

    docker: Add gradle docker plugin

---
+++ messagebroker/kafka-02/src/main/java/com/example/kafka02/Kafka02Application.java
@@ -0,0 +1,13 @@
+package com.example.kafka02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kafka02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kafka02Application.class, args);
+    }
+
+}

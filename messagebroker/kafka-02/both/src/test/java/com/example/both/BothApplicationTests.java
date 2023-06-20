554bc27741fa73e2bef0b2d822a5f8a6956e450a
platanus <platanus@canxan.com>
2023-06-20 16:21:32 +0900

    messagebroker: add project

---
+++ messagebroker/kafka-02/both/src/test/java/com/example/both/BothApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.both;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BothApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

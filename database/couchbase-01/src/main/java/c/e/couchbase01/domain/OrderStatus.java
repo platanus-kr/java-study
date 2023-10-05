456464c70f12f11ac25c0871e2a9f28769b85038
platanus <platanus@canxan.com>
2023-10-05 10:05:06 +0900

    couchbase: ing..

--- database/couchbase-01/src/main/java/c/e/couchbase01/domain/OrderStatus.java
+++ database/couchbase-01/src/main/java/c/e/couchbase01/domain/OrderStatus.java
@@ -1,2 +1,24 @@
-package c.e.couchbase01.domain;public enum OrderStatus {
+package c.e.couchbase01.domain;
+
+
+public enum OrderStatus {
+    PAYED("01", "PAYED"),
+    CANCELED("99", "ORDER CANCELD");
+
+    private final String code;
+    private final String name;
+
+
+    OrderStatus(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
 }

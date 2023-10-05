af7d9313c79f1252642c168feac9734f35d52ad5
platanus <platanus@canxan.com>
2023-10-05 12:12:45 +0900

    concurrent: ConcurrentHashMap cache

---
+++ java-concurrent/src/executors/completablefutures/cacheimplement/Factorizer.java
@@ -0,0 +1,15 @@
+package executors.completablefutures.cacheimplement;
+
+import java.math.BigInteger;
+
+public class Factorizer {
+    private final Computable<BigInteger, BigInteger[]> computable = new Computable<BigInteger, BigInteger[]>() {
+        @Override
+        public BigInteger[] compute(BigInteger arg) throws InterruptedException {
+            return new BigInteger[0];
+        }
+    };
+    private final Computable<BigInteger, BigInteger[]> cache = new Memoizer<>(computable);
+
+    //...
+}

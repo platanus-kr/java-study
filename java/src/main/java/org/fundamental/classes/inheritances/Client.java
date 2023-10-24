55ab0e4379d6a81d00a0d7ab3c39045321624877
platanus <platanus@canxan.com>
2023-10-24 15:51:29 +0900

    java: inheritance practice

--- java/src/main/java/org/fundamental/classes/inheritances/Client.java
+++ java/src/main/java/org/fundamental/classes/inheritances/Client.java
@@ -4,5 +4,7 @@ public class Client {
 	public static void main(String[] args) {
 		WinAmp winAmp = new WinAmp();
 		winAmp.doPreloadByNextFile();
+		
+		System.out.println("왜 너는 이걸 몰라서 다 붙은 면접을 떨어졌니.");
 	}
 }
